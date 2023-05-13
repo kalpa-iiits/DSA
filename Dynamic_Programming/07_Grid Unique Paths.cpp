08957f94c403b4d8cc5d66be68f5cb29e5db1d7b
kalpa-iiits <kalpahe@gmail.com>
2023-05-13 19:48:10 +0530

    Added DP problems

--- Dynamic_Programming/07_Grid Unique Paths.cpp
+++ Dynamic_Programming/07_Grid Unique Paths.cpp
@@ -12,37 +12,9 @@ public:
     }
     int uniquePaths(int m, int n)
     {
-        if (m == 1 || n == 1)
+        if (m == 0 || n == 0)
             return 1;
         vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        return solve(m, n, dp);
+        return solve(m - 1, n - 1, dp);
     }
 };
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
-
-                                                                                 class Solution
-{
-public:
-    int uniquePaths(int m, int n){
-        int uniquePaths(int m, int n){
-            int dp[m + 1][n + 1];
-
-    for (int i = 1; i <= m; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            if (i == 1 || j == 1)
-                dp[i][j] = 1;
-        }
-    }
-    for (int i = 2; i <= m; i++)
-    {
-        for (int j = 2; j <= n; j++)
-        {
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-        }
-    }
-    return dp[m][n];
-}
-}
-;
